8c0e637166303385f973a068b8099067b7214b3f
Lance Becker <lance@superhighway.io>
2023-10-17 16:52:01 -0700

    More UI sugar

--- lua/unatco/plugins/dashboard-nvim.lua
+++ lua/unatco/plugins/dashboard-nvim.lua
@@ -36,7 +36,7 @@ return {
           { action = "Telescope oldfiles", desc = " Recent files", icon = " ", key = "r" },
           { action = "Telescope live_grep", desc = " Find text", icon = " ", key = "g" },
           { action = "e $MYVIMRC", desc = " Config", icon = " ", key = "c" },
-          -- { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
+          { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
           { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
           { action = "qa", desc = " Quit", icon = " ", key = "q" },
         },
