21f0567a7384ad6f8cc232e8ad47af42d2c71e7e
Lance Becker <lance@superhighway.io>
2023-10-20 22:00:31 -0700

    add: vim-illuminate and noice plugins

--- lua/unatco/plugins/mini-indentscope.lua
+++ lua/unatco/plugins/mini-indentscope.lua
@@ -2,9 +2,26 @@ return {
 	"echasnovski/mini.indentscope",
 	version = false,
 	config = function()
-		require("mini.indentscope").setup({
-			symbol = "│",
-			options = { try_as_border = true },
-		})
-	end,
+    require("mini.indentscope").setup({
+      symbol = "│",
+      options = { try_as_border = true },
+    })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
+      callback = function()
+        vim.b.miniindentscope_disable = true
+      end,
+    })
+  end,
 }
